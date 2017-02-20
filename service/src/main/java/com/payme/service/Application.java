adf54d3b311032653c8ba1e4e211c9d1b78bf4f8
Vikash Kumar <vikash.kumar@talentica.com>
2017-02-20 09:09:41 +0530

    fix

--- service/src/main/java/com/payme/service/Application.java
+++ service/src/main/java/com/payme/service/Application.java
@@ -3,16 +3,18 @@ package com.payme.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = { "com.payme" })
 @EnableAutoConfiguration
-@EntityScan("com.payme.common.data.domain")
+@EntityScan(basePackages = { "com.payme.common.data.domain" })
+@EnableJpaRepositories(basePackages = { "com.payme.common.data.repository" })
 @EnableAsync
 @EnableScheduling
 @PropertySource(value = { "classpath:application.properties",
