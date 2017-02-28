556072ac663864250f5f6d0dab519e660a3f7de0
Vikash Kumar <vikash.kumar@talentica.com>
2017-02-28 22:48:15 +0530

    changes

--- dashboard/src/main/java/com/payme/dashboard/controller/UserController.java
+++ dashboard/src/main/java/com/payme/dashboard/controller/UserController.java
@@ -46,13 +46,13 @@ public class UserController {
 	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public void createUser(HttpServletResponse response) throws IOException {
 		Date timeNow = new Date();
-		if (CommonUtil.isNotNull(userRepo.findByEmail("admin@paycr.com"))) {
+		if (CommonUtil.isNotNull(userRepo.findByEmail("admin@paycr.in"))) {
 			return;
 		}
 		PmUser user = new PmUser();
 		user.setCreated(timeNow);
 		user.setName("Vikash Kumar");
-		user.setEmail("admin@paycr.com");
+		user.setEmail("admin@paycr.in");
 		user.setPassword(bcPassEncode.encode("password@123"));
 		user.setMobile("9970197591");
 		List<UserRole> userRoles = new ArrayList<UserRole>();
