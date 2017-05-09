05fb22e73569c34e6e9bbcdad5bdb593d5485721
Vikash Kumar <vikash.kumar@talentica.com>
2017-05-09 14:37:20 +0530

    preAuthProvider lowest precedence

--- service/src/main/java/com/paycr/service/CustomPreAuthProvider.java
+++ service/src/main/java/com/paycr/service/CustomPreAuthProvider.java
@@ -3,12 +3,15 @@ package com.paycr.service;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component("preAuthProvider")
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class CustomPreAuthProvider extends PreAuthenticatedAuthenticationProvider {
 
 	@Autowired
