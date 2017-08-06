a4303857f0f7c45e7de67e8c6685e7470b991af4
Vikash Kumar <vikash.kumar@talentica.com>
2017-08-06 19:26:37 +0530

    try

--- service/src/main/java/com/paycr/service/MvcConfig.java
+++ service/src/main/java/com/paycr/service/MvcConfig.java
@@ -1,5 +1,8 @@
 package com.paycr.service;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.support.ErrorPageFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -14,6 +17,19 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/");
+	}
+
+	@Bean
+	public ErrorPageFilter errorPageFilter() {
+		return new ErrorPageFilter();
+	}
+
+	@Bean
+	public FilterRegistrationBean disableSpringBootErrorFilter(ErrorPageFilter filter) {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(filter);
+		filterRegistrationBean.setEnabled(false);
+		return filterRegistrationBean;
 	}
 
 }
